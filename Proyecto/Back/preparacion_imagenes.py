7ac63b42b6c4adca8e0c4bd60bce9f046d143db4
Plinio Adolfo Parra Cortes (Claro-Comcel) <plinio.parra@ecopetrol.com.co>
2026-09-14 12:16:34 -0500

    V0.1.0

---
+++ Proyecto/Back/preparacion_imagenes.py
@@ -0,0 +1,71 @@
+import cv2
+import numpy as np
+
+from glob import glob # Cuenta cuantas imagenes hay en la carpeta
+
+# 1. Cargar imagen en escala de grises / Preparación
+
+imagenes_carpeta = glob("./Proyecto/Recursos/Imagenes/*.jpg")
+
+def preparacion_img(imagen_a_Procesar):
+    imagen_origin = cv2.imread(imagen_a_Procesar)
+    if imagen_origin is None:
+        print("Imagenes no encontradas")
+    else:
+        imagen_gris = cv2.cvtColor(imagen_origin, cv2.COLOR_BGR2GRAY) # Paso 1 Pipeline: Conversión a escala de grises
+        _, imagen_binaria = cv2.threshold(imagen_gris, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)# Paso 2 Pipeline: Umbralización (Otsu o Binaria Invertida según el fondo) Asumiendo objetos oscuros sobre fondo claro
+
+        
+
+        # Paso 3 Pipeline: Limpieza Morfológica (Apertura para eliminar ruido pequeño y separar contornos)
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+        imagen_limpia = cv2.morphologyEx(imagen_binaria, cv2.MORPH_OPEN, kernel)
+
+        # Paso 4 Pipeline: Detección de Contornos
+        contornos, _ = cv2.findContours(imagen_limpia, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        # Lógica empresarial: Umbral de área "X" para clasificar objetos
+        UMBRAL_AREA_X = 3000  # Píxeles (Ajustar según la resolución de tu imagen)
+
+        print("--- RESULTADOS DE CLASIFICACIÓN DE OBJETOS ---")
+
+        for i, cnt in enumerate(contornos, start=1):
+            # Paso 3 (Requisito): Calcular área
+            area = cv2.contourArea(cnt)
+    
+            # Ignorar ruido insignificante
+            if area < 100:
+                continue
+
+         # Imprimir área en consola
+            print(f"Objeto #{i}: Área = {area:.2f} px²")
+
+            # Bounding box
+            x, y, w, h = cv2.boundingRect(cnt)
+
+            # Paso 4 (Requisito): Lógica empresarial
+            if area > UMBRAL_AREA_X:
+             # Objeto grande -> Bounding Box AZUL en BGR: (255, 0, 0)
+                color_box = (255, 0, 0)
+                etiqueta = "Grande"
+            else:
+                # Objeto pequeño -> Bounding Box ROJO en BGR: (0, 0, 255)
+                color_box = (0, 0, 255)
+                etiqueta = "Pequeno"
+
+            # Dibujar Bounding Box y etiqueta
+            cv2.rectangle(imagen_origin, (x, y), (x + w, y + h), color_box, 2)
+            cv2.putText(imagen_origin, f"{etiqueta} ({int(area)})", (x, y - 8),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, color_box, 1)
+
+
+        return imagen_limpia, imagen_origin
+
+
+
+
+for i in range (len(imagenes_carpeta)+1):
+    imagen_a_tratar = f"./Proyecto/Recursos/Imagenes/img_{i}.jpg"
+    preparacion_img(imagen_a_tratar)
+
+
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # IA2_proyecto_final
+
+-Nombre del proyecto: Reconocimiento facial mediante herramienta IA
+-Breve descripción del proyecto: es una tecnología biométrica que analiza, identifica o verifica la identidad de una persona a partir de los rasgos únicos de su rostro en una imagen o video.
+-carrera: ing software, 5to semestre, Jornada Nocturna
+- universidad: Universitaria de colombia
+-nombre completo de cada integrante 
+Silvia Aguilar
+Zuley Gomez
+Aura aponte
+Gustavo Holguin
+Plinio  Parra
